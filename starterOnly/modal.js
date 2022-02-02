d8b80ebc1b842760aa6ba53ff6c5738ccbb5b97d
Cloé Boudrant <cloeboudrant@protonmail.com>
2022-02-02 09:01:58 +0100

    Javascript sans boolean final d'affichage des messages d'erreur

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,64 +11,137 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const formData = document.querySelectorAll(".formData input[required]");
 const closeModalBtn = document.querySelectorAll(".close");
+const submitbtn = document.querySelector(".btn-submit");
+const modalContent = document.querySelector(".content");
+const gotohead = document.getElementById("myTopnav");
+const modalConfirm = document.querySelector(".modal-body-confirm");
+const modalBody = document.querySelector(".modal-body");
+const champDate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const checkbox = document.getElementById("checkbox1");
+const radio = document.querySelectorAll('input[name="location"]');
+const prenom = document.getElementById("first");
+const nom = document.getElementById("last");
+const email = document.getElementById("email");
+const test = document.querySelectorAll(".formData");
 
-//************************************************************************
-
-// launch modal event
+// Launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
 
-// close modal event
-closeModalBtn.forEach((btn) => btn.addEventListener("click", closeModal));
-
-// close modal form
-function closeModal() {
-  modalbg.style.display = "none";
-}
-
-//*************************************************************************
-
-// Variables du formulaire
-const prenom = document.querySelector("#first");
-const last = document.querySelector("#last");
-const email = document.querySelector("#email");
-const birthdate = document.querySelector("#birthdate");
-const quantity = document.querySelector("#quantity");
-const location1 = document.querySelector("#location1");
-const location2 = document.querySelector("#location2");
-const location3 = document.querySelector("#location3");
-const location4 = document.querySelector("#location4");
-const location5 = document.querySelector("#location5");
-const location6 = document.querySelector("#location6");
-const checkbox1 = document.querySelector("#checkbox1");
-const checkbox2 = document.querySelector("#checkbox2");
-
-
-
-
-
-document.getElementById("inscription").addEventListener("submit", function(e)
+// Functions
+function launchModal() 
 {
-  if (!prenom.value)
-  {
-    erreur = "Veuillez saisir votre prénom";
-  }
-  if (erreur)
-  {
-    e.preventDefault();
-    document.getElementById("erreur").innerHTML = erreur;
-    return false;
-  }
-  else
-  {
-    alert('Formulaire envoyé !');
-  }
-});
+  gotohead.scrollIntoView({behavior: "auto", block:"center", inline:"nearest"});
+  modalbg.style.display = "block";
+  closeModalBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+  submitbtn.addEventListener("click", validate);
+  prenom.addEventListener("input", prenomValid);
+  nom.addEventListener("input", nomValid);
+  email.addEventListener("input", emailValid);
+  champDate.addEventListener("input", dateValid);
+  radio.forEach((input) => input.addEventListener("change", radioValid));
+}
 
+function closeModal()
+ {
+  modalbg.style.display = "none";
+  closeModalBtn.forEach((btn) => btn.removeEventListener("click", closeModal));
+  modalbg.removeEventListener("click", closeModal);
+  submitbtn.removeEventListener("click", validate);  
+  prenom.removeEventListener("input", prenomValid);
+  nom.removeEventListener("input", nomValid);
+  email.removeEventListener("input", emailValid);
+  champDate.removeEventListener("input", dateValid);
+  radio.forEach((input) => input.removeEventListener("change", radioValid));
+  modalBody.style.display = "block";
+  modalConfirm.style.display = "none";
+  reinit();
+}
 
+function reinit() {
+  prenom.value = "";
+  nom.value = "";
+  email.value = "";
+  champDate.value = "";
+  quantity.value = "";
+  radio.forEach((input) => input.checked = false);
+  checkbox.checked = false;
+  test.forEach((formData) => formData.dataset.errorVisible = false);
+}
+
+let prenomFinal = "false";
+
+function prenomValid(e) {
+  let prenomTest = e.currentTarget.checkValidity();
+  if (prenomTest == false) {
+    e.currentTarget.parentElement.dataset.errorVisible = true;
+    prenomFinal = false;
+  } else {
+    e.currentTarget.parentElement.dataset.errorVisible = false;
+    prenomFinal = true;
+  }
+}
+
+let nomFinal = "false";
+
+function nomValid(e) {
+  let nomTest = e.currentTarget.checkValidity();
+  if (nomTest == false) {
+    e.currentTarget.parentElement.dataset.errorVisible = true;
+    nomFinal = false;
+  } else {
+    e.currentTarget.parentElement.dataset.errorVisible = false;
+    nomFinal = true;
+  }
+}
+
+let emailFinal = "false";
+
+function emailValid(e) {
+  let emailTest = e.currentTarget.checkValidity();
+  if (emailTest == false) {
+    e.currentTarget.parentElement.dataset.errorVisible = true;
+    emailFinal = false;
+  } else {
+    e.currentTarget.parentElement.dataset.errorVisible = false;
+    emailFinal = true;
+  }
+}
+
+let dateFinal = "false";
+
+function dateValid(e) {
+let dateFormat = /^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$/;
+if (e.target.value.match(dateFormat)) {
+  e.currentTarget.parentElement.dataset.errorVisible = false;
+  dateFinal = true;
+} else {
+  e.currentTarget.parentElement.dataset.errorVisible = true;
+  dateFinal = false;
+  }
+}
+
+let radioFinal = "false";
+
+function radioValid() {
+    radioFinal = true;
+}
+
+quantityFinal = "false";
+checkboxFinal = "false";
+
+function validate(e) {
+  e.preventDefault();
+  quantityFinal = quantity.checkValidity();
+  checkboxFinal = checkbox.checkValidity();
+  if (prenomFinal == true && nomFinal == true && emailFinal == true && dateFinal == true && quantityFinal == true && radioFinal == true && checkboxFinal == true) {
+    modalBody.style.display = "none";
+    modalConfirm.style.display = "flex";
+    return true;
+  } else {
+    console.log("Nunuche");
+  }
+}
